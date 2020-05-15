9dc6e5ffbf4c060dfa1ab31fb962dceef5e5a7a4
Sierra Curtis <sierracurtis91@gmail.com>
2020-05-15 13:51:00 -0400

    finished mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,22 +7,25 @@ class RingBuffer:
         self.current = 0
 
     def append(self, item):
-        # if length is less than capacity
+        # if storage length is less than capacity
         if self.storage.length < self.capacity:
             # add item to the tail
             self.storage.add_to_tail(item)
             # set current to current the tail
             self.current = self.storage.tail
-        # if current is tail
-        if self.current == self.storage.tail:
-            # set current to the head
-            self.current = self.storage.head
-        # if current is head
-        # elif self.current == self.storage.head:
+        # if storage length is greater than capacity
         else:
-            # set the current to the next value
-            self.current = self.current.next
-        self.current.value = item
+            # if current is tail
+            if self.current == self.storage.tail:
+                # set current to the head
+                self.current = self.storage.head
+            # if current is head
+            # elif self.current == self.storage.head:
+            else:
+                # set the current to the next value
+                self.current = self.current.next
+            # else: set item to current value
+            self.current.value = item
 
     def get(self):
         # make a list
